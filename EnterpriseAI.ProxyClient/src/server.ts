c01813d3db321e7b71b905c74b7618882936e30a
Andrew Petersen <andrew.petersen15@gmail.com>
2024-12-26 09:59:17 -0600

    got it working hitting the azure deployed api via .env and with https

--- EnterpriseAI.ProxyClient/src/server.ts
+++ EnterpriseAI.ProxyClient/src/server.ts
@@ -1,13 +1,17 @@
 import { Hono } from "hono";
 import fs from "node:fs";
+import https from "node:https";
 const app = new Hono();
 
 const config = {
-  apiUrl: process.env.API_URL ?? "http://localhost:5229",
-  chatRoute: process.env.CHAT_ROUTE ?? "/chat/submit",
-  apiKey: process.env.API_KEY ?? "my-api-key",
-  apiKeyHeader: process.env.API_KEY_HEADER ?? "X-API-KEY",
+  apiUrl: import.meta.env.VITE_API_URL ?? "http://localhost:5229",
+  chatRoute: import.meta.env.VITE_CHAT_ROUTE ?? "/chat/submit",
+  apiKey: import.meta.env.VITE_API_KEY ?? "my-api-key",
+  apiKeyHeader: import.meta.env.VITE_API_KEY_HEADER ?? "X-API-KEY",
+  isDevelopment: import.meta.env.NODE_ENV !== "production",
 };
+
+console.log("🚀 | USING CONFIG: ", config);
 
 // Serve static files from the root directory
 
@@ -27,7 +31,22 @@ app.post("/api/chat-proxy", async (c) => {
     duplex: "half",
   });
 
-  return fetch(newRequest);
+  const fetchOptions = config.isDevelopment
+    ? {
+        agent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      }
+    : undefined;
+
+  let response = await fetch(newRequest, fetchOptions as any);
+  if (!response.ok) {
+    return Response.json(
+      { error: "Failed to call Proxied Chat API", body: await response.text() },
+      { status: response.status }
+    );
+  }
+  return Response.json(await response.json());
 });
 
 export default app;
